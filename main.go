75982492fb34e6fffebb0c196c9ed5762b8dfa24
碧海青天 <74009293+bh-qt@users.noreply.github.com>
2024-07-25 14:21:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	asn2 "github.com/jackrun123/cfiptest/pkgs/asn"
-	"github.com/jackrun123/cfiptest/pkgs/speed"
+	asn2 "github.com/bh-qt/iptest/pkgs/asn"
+	"github.com/bh-qt/iptest/speed"
 	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
